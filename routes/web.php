635bbef0424f5870dbd26b4bb47fd5f63230cf4e
Jirayus Arbking <dev.jirayus@gmail.com>
2024-01-29 22:32:55 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\C_titles;
+use App\Http\Controllers\MyAuth;
 use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,8 +18,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('titles', C_titles::class);
+Route::get('/login', [MyAuth::class, 'login_view'])->name('login');
+Route::get('/register', [MyAuth::class, 'register_view']);
+Route::get('/logout', [MyAuth::class, 'logout_process']);
+Route::post('/login', [MyAuth::class, 'login_process']);
+Route::post('/register', [MyAuth::class, 'register_process']);
 
+Route::resource('titles', C_titles::class)->middleware('auth');
+Route::middleware('auth')->group(function(){
+    // auth first
+});
 Route::get('/my-controller', [MyController::class, 'index']);
 
 Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
